b019f16f9ebfd36a5c2d5d2b30fd32697506dfdf
HexMerlin <alec-sweden@hotmail.com>
2023-11-29 23:14:55 +0100

    Added method

--- OpenSvg/PointExtensions.cs
+++ OpenSvg/PointExtensions.cs
@@ -10,6 +10,30 @@ public static class PointExtensions
 {
 
     /// <summary>
+    /// Determines whether the specified point is within a certain distance from the current point.
+    /// </summary>
+    /// <remarks>
+    /// This method is significantly faster than <see cref="Point.Distance(Point, Point)"/> since it avoids computing the actual distance.
+    /// </remarks>
+    /// <param name="point">The current point.</param>
+    /// <param name="other">The point to compare with the current point.</param>
+    /// <param name="distance">The distance threshold.</param>
+    /// <returns>
+    /// <c>true</c> if the specified point is within the given distance from the current point; otherwise, <c>false</c>.
+    /// </returns>
+    /// <example>
+    /// <code>
+    /// Point p1 = new Point(0, 0);
+    /// Point p2 = new Point(3, 4);
+    /// double distance = 5;
+    /// bool isWithin = p1.IsWithinDistance(p2, distance);
+    /// Console.WriteLine(isWithin);
+    /// //Output: <c>True</c> (since p2 is exactly 5 units away from p1)
+    /// </code>
+    /// </example>
+    public static bool IsWithinDistance(this Point point, Point other, double distance) => Point.DistanceSquared(point, other) <= distance * distance;
+   
+    /// <summary>
     /// Transforms the point by the specified transform.
     /// </summary>
     /// <param name="point">The point to transform.</param>
@@ -28,13 +52,14 @@ public static class PointExtensions
     {
         const float Tolerance = 1e-5f;
 
-        // Check if point is within the bounding box of the line segment
+        // Return false if the point is outside the bounding box of the line segment
         if (p.X < MathF.Min(a.X, b.X) || p.X > MathF.Max(a.X, b.X)) return false;
         if (p.Y < MathF.Min(a.Y, b.Y) || p.Y > MathF.Max(a.Y, b.Y)) return false;
 
         // Check if point is collinear with line segment using cross-product
         float crossProduct = (p.Y - a.Y) * (b.X - a.X) - (p.X - a.X) * (b.Y - a.Y);
         return MathF.Abs(crossProduct) <= Tolerance;
+
     }
 
     /// <summary>
